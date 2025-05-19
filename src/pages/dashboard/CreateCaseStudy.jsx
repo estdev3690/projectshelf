38f4753849719cf28536a2b28ff99698edb6d139
estdev <estdev123@gmail.com>
2025-05-19 10:00:47 +0530

    cloudinary changes

--- src/pages/dashboard/CreateCaseStudy.jsx
+++ src/pages/dashboard/CreateCaseStudy.jsx
@@ -256,13 +256,14 @@ const CreateCaseStudy = () => {
       
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+      formData.append('upload_preset', 'portfolio_uploads'); //
 
       try {
-        const response = await fetch(`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`, {
+        const response = await fetch(`https://api.cloudinary.com/v1_1/dyznk0c2q/image/upload`, {
           method: 'POST',
           body: formData,
         });
+
 
         const data = await response.json();
         console.log('Cloudinary URL:', data.secure_url);
